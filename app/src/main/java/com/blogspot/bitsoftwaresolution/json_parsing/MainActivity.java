1b86507682be74b8c81006b05e95026f886556e7
Rakesh <rakeshsutar85@gmail.com>
2020-11-04 14:35:49 +0530

    ADDING

--- app/src/main/java/com/blogspot/bitsoftwaresolution/json_parsing/MainActivity.java
+++ app/src/main/java/com/blogspot/bitsoftwaresolution/json_parsing/MainActivity.java
@@ -18,9 +18,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
